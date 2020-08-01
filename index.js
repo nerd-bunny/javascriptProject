941ce407b1a3a851e1ef4c91dd672f9a7fa76db0
The Guy You Don't Know <contact@vanditparekh.com>
2020-08-01 13:54:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
+//Name: Vandit Parekh
+//Student No.: 200412374
+
+//Variable to store the user inputed value from the text box
 let addItem = document.getElementById('inputText');
+//Variable to access the submit button and call a onclick method
 let submitButton = document.getElementById('submitButton');
+//Variable to access the division to add ToDo's
 let idforToDo = document.getElementById('idforToDo');
+//Variable to access the division of the ToDo's marked: checked
 let completedItem = document.getElementById('completedItem');
 
+//Submit Button and onclick
 submitButton.onclick = getToDoItem;
 
+//Sort of a main method, where all the operations for this specific application occurs
 function getToDoItem(){
+    //Getting the value of the user inputed text, from the textbox
     let todo = addItem.value;
+    //Creating 4 elements to present the user the ToDo's in a formatted manner
     let todoItem = document.createElement('input');
     let todoItemLabel = document.createElement('label');
     let delButton = document.createElement('button');
     let newline = document.createElement('br');
 
+    //Setting the value of the textbox to null
     addItem.value = null;
-
+    //Setting the input type as check box
     todoItem.type = "checkbox";
+    //Setting up the input field
     todoItem.id = "todoListItem";
+    //Relating and appending the label element to the input element
     todoItemLabel.htmlFor = todoItem;
     todoItemLabel.appendChild(document.createTextNode(todo));
-
-    appendChildFunction("idforToDo");
-
+    //Setting up the Delete button
     delButton.innerHTML = "Delete";
     delButton.setAttribute('id', 'delButton');
 
+    //Goes to this function to add the elements to the page
+    appendChildFunction("idforToDo");
+
+    //Setting an onclick listener on the delete button
     delButton.onclick = function(){
+        //If button is clicked, it goes to this function to delete the elements
         removeChildFunction("idforToDo");
     }
-
+    //If the checkbox is clicked this method is called which in turn runs a function
     todoItem.onchange = function (e){
-
+        //This sends the completed ToDo to the completed ToDO division and adds some style to it
         appendChildFunction("completedItem");
         todoItemLabel.style.textDecoration = "line-through";
-
+        //Specifying actions that should take place if delete button is clicked
         delButton.onclick = function(){
             removeChildFunction("completedItem");           
         }
-
+        //If the checkbox is clicked again, it sends the elements back up above and removes the style added to it
         todoItem.onchange = function(e){
             appendChildFunction("idforToDo");
             todoItemLabel.style.textDecoration = "none";
@@ -47,6 +64,7 @@ function getToDoItem(){
         }
     }
 
+    //This function depending on where it was called from adds element to the screen
     function appendChildFunction(appendParentID) {
         if(appendParentID === "idforToDo"){
             idforToDo.appendChild(todoItem);
@@ -61,7 +79,7 @@ function getToDoItem(){
             completedItem.appendChild(newline);
         }
     }
-
+    //This function depending on where it was called from removes/deletes element from the screen
     function removeChildFunction(removeParentID) {
         if(removeParentID === "idforToDo"){
             idforToDo.removeChild(todoItemLabel);
@@ -77,3 +95,4 @@ function getToDoItem(){
         }
     }
 }
+//End of Javascript file
